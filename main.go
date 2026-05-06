c0abda75bd978350fb960ef575f71e74e5a8098e
Samuel Keeley <samuel@keeley.net>
2026-05-06 12:17:21 -0500

    main: accept --debug flag for attaching delve / IDE debuggers
    terraform-plugin-framework's providerserver supports a Debug mode where it prints a TF_REATTACH_PROVIDERS line on startup and waits, allowing a debugger (delve, the Terraform LSP/IDE integrations, etc.) to attach to the running provider before Terraform invokes any function. Without --debug wired up, the provider just runs to completion and there's no stable attach point.
    
    Networkutils' main.go had this from day one; burnham's never did. Symmetric with how every other terraform-plugin-framework provider's main.go is structured.
    
    Five-line change: add a flag.BoolVar, pass Debug into ServeOpts.
    
    Signed-off-by: Samuel Keeley <samuel@keeley.net>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -11,8 +12,14 @@ import (
 )
 
 func main() {
+	// `--debug` lets a debugger (delve, IDE-attached debuggers, etc.) attach to the running provider. terraform-plugin-framework's providerserver prints a TF_REATTACH_PROVIDERS line on startup that the editor's Terraform integration can pick up.
+	var debug bool
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	err := providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/keeleysam/burnham",
+		Debug:   debug,
 	})
 	if err != nil {
 		log.Fatal(err)
